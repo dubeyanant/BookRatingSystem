193b3b45a815729c231d1250956e7243a54e40b9
Anant Dubey <anantkumar312@gmail.com>
2023-01-02 14:10:31 +0530

    Added Book Value column

--- first.R
+++ first.R
@@ -1,1 +1,16 @@
 Books <- read.csv(paste(as.character(getwd()),"/info.csv", sep=""))
+# Four columns in the Books data set
+# Name, Page Count, Book Rating, Number of Ratings
+
+# Find the Book Value by an algo discussed here: https://stackoverflow.com/a/50476254/14224859
+
+# We first determine the value of Default Rating by finding the sample median
+R <- median(Books$Rating)
+# Now we find Worth of the Default Rating
+W <- R/10
+
+# Formula for finding the Book Value: BV = (W*R + NOR*Rating) / (W+NOR)
+Books$BV <- ((W*R)+(Books$NOR*Books$Rating))/(W+Books$NOR)*2
+Books$BV <- trunc(Books$BV*10^2)/10^2
+
+# Plotting the graph of Book Value vs Page Count
