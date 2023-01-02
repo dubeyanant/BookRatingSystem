a7a5a6e51bcdf716377363c1b83176a39a9e1edf
Anant Dubey <anantkumar312@gmail.com>
2023-01-02 16:21:20 +0530

    corrected value of R and W

--- first.R
+++ first.R
@@ -5,13 +5,14 @@ Books <- read.csv(paste(as.character(getwd()),"/info.csv", sep=""))
 # Find the Book Value by an algo discussed here: https://stackoverflow.com/a/50476254/14224859
 
 # We first determine the value of Default Rating by finding the sample median
-R <- median(Books$Rating)
+#R <- median(Books$Rating)
+R <- 2.4
 # Now we find Worth of the Default Rating
-W <- R/10
+W <- 500
 
 # Formula for finding the Book Value: BV = (W*R + NOR*Rating) / (W+NOR)
-Books$BV <- ((W*R)+(Books$NOR*Books$Rating))/(W+Books$NOR)*2
-Books$BV <- trunc(Books$BV*10^2)/10^2
+Books$BV <- (((W*R)+(Books$NOR*Books$Rating))/(W+Books$NOR))*2
+#Books$BV <- trunc(Books$BV*10^2)/10^2
 
 Books <- Books[order(Books$BV, decreasing = TRUE), ]
 
@@ -20,9 +21,8 @@ Books <- Books[order(Books$BV, decreasing = TRUE), ]
 #install.packages("gghighlight")
 library("ggplot2")
 library("gghighlight")
-ggplot(data = Books,
-       mapping = aes(x = Page,
-                     y = BV)) + geom_point() + 
-  geom_label(aes(label=Name), nudge_y = 0.05, size = 3) + 
+ggplot(data = Books, mapping = aes(x = Page, y = BV,)) +
+  geom_point(color = "red") + 
+  geom_label(aes(label=Name), nudge_y = 0.08, size = 3) + 
   labs(x = "Page Count", y = "Book Value") + 
-  gghighlight(BV > 8, Page < 250, label_key = type)
+  gghighlight(BV > 7.5, Page < 250, label_key = type)
