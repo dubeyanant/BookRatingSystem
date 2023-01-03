69a199d81d2f780b316756889f6067492d8b4c76
Anant Dubey <anantkumar312@gmail.com>
2023-01-03 20:26:53 +0530

    cleaned some code

--- first.R
+++ first.R
@@ -5,27 +5,24 @@ Books <- read.csv(paste(as.character(getwd()),"/info.csv", sep=""))
 # Find the Book Value by an algo discussed here: https://stackoverflow.com/a/50476254/14224859
 
 # We first determine the value of Default Rating by finding the sample median
-#R <- median(Books$Rating)
+# R <- median(Books$Rating)
 R <- 2.4
-# Now we find Worth of the Default Rating
-W <- 500
+W <- 400 # Now we find Worth of the Default Rating
 
-# Formula for finding the Book Value: BV = (W*R + NOR*Rating) / (W+NOR)
-Books$BV <- (((W*R)+(Books$NOR*Books$Rating))/(W+Books$NOR))*2
-Books$BV <- trunc(Books$BV*10^2)/10^2
+Books$BV <- (((W*R)+(Books$NOR*Books$Rating))/(W+Books$NOR))*2 # Finding Book Value
+Books$BV <- trunc(Books$BV*10^2)/10^2 # Truncating to two decimal points
+Books <- Books[order(Books$BV, decreasing = TRUE),] # Sorting on the basis of BV
 
-Books <- Books[order(Books$BV, decreasing = TRUE), ]
-
-# Plotting the graph of Book Value vs Page Count
 #install.packages("ggplot2")
 #install.packages("gghighlight")
 #install.packages("ggrepel")
 library("ggrepel") 
 library("ggplot2")
 library("gghighlight")
+
+# Plotting the graph of Book Value vs Page Count
 ggplot(data = Books, mapping = aes(x = Page, y = BV,)) +
   geom_point(color = "red") + 
-  geom_label_repel(aes(label=Name), nudge_y = 0.04, size = 3) + 
+  geom_text_repel(aes(x = Page, y = BV, label = Name), nudge_y = 0.04) + 
   labs(x = "Page Count", y = "Book Value") + 
-  gghighlight(BV > 7.7, Page < 200, label_key = type)
-#+ geom_text_repel(aes(x = Page, y = BV, label = Name))
+  gghighlight(BV > 7.7, Page < 250, label_key = type)
